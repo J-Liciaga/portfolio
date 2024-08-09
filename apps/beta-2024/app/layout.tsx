3461794565d6352ab89e21322fc49ad3bc64e806
Josiah Liciaga <sensei@jarvis.attlocal.net>
2024-08-08 21:43:01 -0400

    chore(beta-2024): added speed insights

--- apps/beta-2024/app/layout.tsx
+++ apps/beta-2024/app/layout.tsx
@@ -5,6 +5,7 @@ import {
 	FooterNavigation,
 } from "@2024/components/navigation";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Toaster from "@lucky-ui/components/toaster";
 
 export const metadata = {
@@ -25,6 +26,7 @@ export default async function RootLayout({
 				<main className="min-h-screen min-w-screen">{children}</main>
 				<FooterNavigation />
 				<Analytics />
+				<SpeedInsights />
 				<Toaster />
 			</body>
 		</html>
