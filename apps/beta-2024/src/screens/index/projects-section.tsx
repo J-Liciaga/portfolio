af70d3a472f7063d3da831740a5400edcdbe9aec
Josiah Liciaga <sensei@jarvis.attlocal.net>
2024-08-10 16:19:01 -0400

    feat(beta-2024): i18n refactor in progress

--- apps/beta-2024/src/screens/index/projects-section.tsx
+++ apps/beta-2024/src/screens/index/projects-section.tsx
@@ -15,6 +15,7 @@ type ProjectsSectionProps = {
 const styles = {
 	container: "h-fit w-screen",
 	content_wrapper: "h-full w-full | px-12 space-y-8",
+	// wobble
 	card_box: "w-full | grid grid-cols-1 lg:grid-cols-3 gap-4",
 	card_content_wrapper: "max-w-xs",
 	card_label:
@@ -22,6 +23,10 @@ const styles = {
 	card_desc: "mt-4 text-left  text-base/6 text-zinc-200",
 	logo_img:
 		"hidden md:block absolute -right-4 lg:-right-[25%] grayscale filter -bottom-10 object-contain rounded-2xl",
+	card_nav:
+		"absolute bottom-4 flex justify-start items-center pt-6 space-x-6",
+	card_nav_status: "bg-transparent text-white border-white",
+	card_nav_visit_link: "flex justify-center items-center | space-x-2",
 } as const;
 
 export default function ProjectsSection({ lng }: ProjectsSectionProps) {
@@ -44,12 +49,12 @@ export default function ProjectsSection({ lng }: ProjectsSectionProps) {
 						className={styles.logo_img}
 					/>
 				)}
-				<div className="absolute bottom-4 flex justify-start items-center pt-6 space-x-6">
-					<div className="bg-transparent text-white border-white">
+				<div className={styles.card_nav}>
+					<div className={styles.card_nav_status}>
 						Status: Ongoing
 					</div>
 					<LinkPreview url="https://framer.com/motion">
-						<div className="flex justify-center items-center | space-x-2">
+						<div className={styles.card_nav_visit_link}>
 							<div>VISIT</div>
 							<IconExternalLink />
 						</div>
